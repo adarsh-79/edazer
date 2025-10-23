8593d949e8b10a383a6054be253d7c7a2ef87a58
Adarsh R <adarsh3690704@gmail.com>
2025-10-23 23:42:37 +0530

    added new function for primary key column identification, moved idf.py into utils

--- edazer/__init__.py
+++ edazer/__init__.py
@@ -3,6 +3,8 @@ Provides functionalities for common EDA tasks
 '''
 
 from .core import Edazer
-from .idf import interactive_df
+
+from .utils.idf import interactive_df
 #from .profiling import show_data_profile
 
+from .utils.pk import get_primary_key
